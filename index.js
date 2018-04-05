38052deacfe276d3242932b19ab04971069fbb12
Carl Youngblood <carl.youngblood@shipstation.com>
2018-04-05 12:44:39 -0500

    Add template route to server

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const route_tempate = require('./src/routes/template')
+const template_route_builder = require('./src/routes/template')
 const server = require('./src/server')
 
-server.launch(route_tempate)
+const template_route = template_route_builder(async (name) => {
+  if (name === '/notfound') {
+    const error = new Error()
+    error.not_found = true
+    throw error
+  }
+  return 'butts'
+})
+
+server.launch(template_route)
   .then(() => console.log('Server starting'))
   .catch((err) => console.log(`Server startup failed: ${err}`))
--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const koa = require('koa')
 
 const app = new koa()
 
-const launch = async (route_template) => {
+const launch = async (template_route) => {
   app.listen(3000)
-  app.use(route_template)
+  app.use(template_route)
 }
 
 module.exports = {
